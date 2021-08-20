5fb725eaba75ac93fde5cd7adb4a3589e1bc30e8
Saravanan <33788369+sharvanan@users.noreply.github.com>
2021-08-20 17:28:38 +0800

    Component resue

--- components/src/index.js
+++ components/src/index.js
@@ -2,28 +2,41 @@ import React from "react";
 import ReactDOM from "react-dom";
 import faker from "faker";
 import CommentDetail from "./CommentDetail";
+import ApprovalCard from "./ApprovalCard";
 
 const App = () => {
   return (
     <div className="ui container comments">
-      <CommentDetail
-        author="Sam"
-        timeAgo="Today at 2:00 AM"
-        avatar={faker.image.avatar()}
-        content="Able to understand props"
-      />
-      <CommentDetail
-        author="Alex"
-        timeAgo="Today at 11:00 AM"
-        avatar={faker.image.avatar()}
-        content="Props are passed from parent to child"
-      />
-      <CommentDetail
-        author="Jamie"
-        timeAgo="Yesterday at 5:00 PM"
-        avatar={faker.image.avatar()}
-        content="Nice learning"
-      />
+      <ApprovalCard>
+        <h4>Warning !!</h4>
+        <div>
+          Are you sure?
+        </div>
+      </ApprovalCard>
+      <ApprovalCard>
+        <CommentDetail
+          author="Sam"
+          timeAgo="Today at 2:00 AM"
+          avatar={faker.image.avatar()}
+          content="Able to understand props"
+        />
+      </ApprovalCard>
+      <ApprovalCard>
+        <CommentDetail
+          author="Alex"
+          timeAgo="Today at 11:00 AM"
+          avatar={faker.image.avatar()}
+          content="Props are passed from parent to child"
+        />
+      </ApprovalCard>
+      <ApprovalCard>
+        <CommentDetail
+          author="Jamie"
+          timeAgo="Yesterday at 5:00 PM"
+          avatar={faker.image.avatar()}
+          content="Nice learning"
+        />
+      </ApprovalCard>
     </div>
   );
 };
