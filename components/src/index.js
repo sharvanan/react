76bf83bdfb59dc72a388b840b606bfbc5531c284
Saravanan <33788369+sharvanan@users.noreply.github.com>
2021-08-20 17:07:45 +0800

    Props sending and receiving 2

--- components/src/index.js
+++ components/src/index.js
@@ -1,15 +1,31 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import CommentDetail from './CommentDetail';
+import React from "react";
+import ReactDOM from "react-dom";
+import faker from "faker";
+import CommentDetail from "./CommentDetail";
 
-const App = () =>{
-    return (
-<div className = "ui container comments">
-  <CommentDetail author="Sam" />
-  <CommentDetail author="Alex" />
-  <CommentDetail author="Jamie" />
-</div>
-    );
+const App = () => {
+  return (
+    <div className="ui container comments">
+      <CommentDetail
+        author="Sam"
+        timeAgo="Today at 2:00 AM"
+        avatar={faker.image.avatar()}
+        content="Able to understand props"
+      />
+      <CommentDetail
+        author="Alex"
+        timeAgo="Today at 11:00 AM"
+        avatar={faker.image.avatar()}
+        content="Props are passed from parent to child"
+      />
+      <CommentDetail
+        author="Jamie"
+        timeAgo="Yesterday at 5:00 PM"
+        avatar={faker.image.avatar()}
+        content="Nice learning"
+      />
+    </div>
+  );
 };
 
-ReactDOM.render(<App />, document.querySelector('#root'));
+ReactDOM.render(<App />, document.querySelector("#root"));
