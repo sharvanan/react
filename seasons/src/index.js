b4535c55148f8db74e24a617f7c7befbe7bc7fb0
Saravanan <33788369+sharvanan@users.noreply.github.com>
2021-08-21 11:40:17 +0800

    Replace function with class

--- seasons/src/index.js
+++ seasons/src/index.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import ReactDOM  from 'react-dom';
 
-const App = () => {
-    window.navigator.geolocation.getCurrentPosition(
-        (position) => console.log(position),
-        (err)  => console.log(err)
-    );
-    return <div> hi</div>
-};
+
+class App extends React.Component{
+
+    render(){
+        window.navigator.geolocation.getCurrentPosition(
+            (position) => console.log(position),
+            (err)  => console.log(err)
+        );
+        return <div> hi there</div>
+    }
+}
 
 ReactDOM.render(<App />, document.querySelector('#root'));
